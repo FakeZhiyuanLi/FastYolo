a54eaa392d1fab29c124e5e2f3bc13adc59c592d
liz-mvla <100027902@mvla.net>
2024-04-06 19:01:55 -0700

    code save

--- src/main.py
+++ src/main.py
@@ -1,14 +1,20 @@
 from tkinter import *
 from PIL import Image
 import customtkinter
+import sys
+import os
 
 customtkinter.set_appearance_mode("System")
 customtkinter.set_default_color_theme("blue")
+
+basePath = f'/Users/zhiyuan/Desktop/ThomasTheDankEngineCode/Python/ML/FastYolo/data/project1'
 
 imageIndex = 0
 classes = []
 imageHovering = False
 x, y = 0, 0
+numImages = len(os.listdir(f'{basePath}/rawImages'))
+rawImageLocations = os.listdir(f'{basePath}/rawImages')
 
 def setHovering(self):
     global imageHovering
@@ -17,6 +23,18 @@ def setHovering(self):
 def setNotHovering(self):
     global imageHovering
     imageHovering = False
+
+def incrementImageIndex(self):
+    global imageIndex
+    imageIndex += 1
+    if imageIndex >= numImages:
+        imageIndex = 0
+
+def decrementImageIndex(self):
+    global imageIndex
+    imageIndex = imageIndex-1;
+    if imageIndex < 0:
+        imageIndex = numImages-1
 
 class classPicker(customtkinter.CTkScrollableFrame):
     def __init__ (self, parent, title, values):
@@ -63,12 +81,21 @@ class App(customtkinter.CTk):
         self.classEntry = customtkinter.CTkEntry(self, placeholder_text="Class Name")
         self.classEntry.grid(row=2, column=0, padx=10, pady=10, sticky="ew", columnspan=2)
 
-        self.imageData = customtkinter.CTkImage(light_image=Image.open("/Users/zhiyuan/Desktop/ThomasTheDankEngineCode/Python/ML/FastYolo/data/project1/rawImages/IMG_4874.JPG").resize((640, 640)).rotate(-90), size=(640, 640))
+        self.imageData = customtkinter.CTkImage(light_image=Image.open(f'{basePath}/rawImages/{rawImageLocations[imageIndex]}').resize((640, 640)).rotate(-90), size=(640, 640))
         self.mainImage = customtkinter.CTkLabel(self, image=self.imageData, text="")
         self.mainImage.grid(row=0, column=2, padx=10, pady=10, sticky="ew")
 
         self.mainImage.bind("<Enter>", setHovering)
         self.mainImage.bind("<Leave>", setNotHovering)
+
+        self.backArrow = customtkinter.CTkButton(self, text="← Previous Image", command=self.backEvent)
+        self.backArrow.grid(row=2, column=2, padx=10, pady=10, sticky="ew")
+
+        self.forwardArrow = customtkinter.CTkButton(self, text="→ Next Image", command=self.forwardEvent)
+        self.forwardArrow.grid(row=3, column=2, padx=10, pady=10, sticky="ew")
+
+        self.imageIndexLabel = customtkinter.CTkLabel(self, text=f'Image {imageIndex+1}/{numImages}')
+        self.imageIndexLabel.grid(row=1, column=2, padx=10, pady=10, sticky="ew")
 
         # self.canvas = customtkinter.CTkCanvas(self, width=640, height=640, bg='#000000')
         # self.canvas.grid(row=0, column=2)
@@ -81,11 +108,25 @@ class App(customtkinter.CTk):
         classes.append(className)
         self.classPicker.addClass(className)
 
+    def backEvent(self):
+        decrementImageIndex(self)
+        self.updateImage()
+
+    def forwardEvent(self):
+        incrementImageIndex(self)
+        self.updateImage()
+
+    def updateImage(self):
+        self.imageData.configure(light_image=Image.open(f'{basePath}/rawImages/{rawImageLocations[imageIndex]}').resize((640, 640)).rotate(-90))
+        self.mainImage = customtkinter.CTkLabel(self, image=self.imageData, text="")
+        self.mainImage.grid(row=0, column=2, padx=10, pady=10, sticky="ew")
+        self.imageIndexLabel.configure(text=f'Image {imageIndex+1}/{numImages}')
+
 
 def getMouseLocation(e):
     x = e.x
     y = e.y
-    print(x, y, imageHovering)
+    print(x, y, imageHovering, imageIndex)
 
 if __name__ == '__main__':
     app = App()
